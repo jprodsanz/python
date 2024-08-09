86e86710c226e577f5f84228709b3b6ef042b074
Pablo X <jprodsanz@gmail.com>
2024-08-09 01:05:06 -0500

    nameBandGenCommpleted

---
+++ bandNameGen.py
@@ -0,0 +1,9 @@
+# In this exercise I created a band name generator
+
+print("Hi, you are tuned to DAWN FM, the middle of nowhere on your dial. You might be here a while...")
+
+# By Creating a VAR we will have access to what the user types in
+city = input("What city are you from?\n")
+pet = input("What is the name of your pet?\n")
+
+print("Your band name could be:" " " + city + " " + pet)
